ca76fc8108491a58bc7254d7b9b98a5769a8faef
evgeny-isupov <111366627+evgeny-isupov@users.noreply.github.com>
2022-11-01 16:11:58 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "cloud_access_token" {
   type = string
   sensitive = true
 }
+
+variable "cloud_access_cloud_id" {
+  type = string
+  sensitive = true
+}
+
+variable "cloud_access_folder_id" {
+  type = string
+  sensitive = true
+}
